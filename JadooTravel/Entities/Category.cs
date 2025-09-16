9a995c1de7e04082777d5bd81829bfa74aab9965
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-09-16 21:46:53 +0300

    Add project files.

---
+++ JadooTravel/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace JadooTravel.Entities
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IconUrl { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
